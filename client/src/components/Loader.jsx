bc0770964475baa99c61e4073b34a5d8772039cf
Shivani Mishra <shivanimishra7860123@gmail.com>
2026-06-13 19:06:59 +0530

    fix useEffect import in loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAppContext } from '../context/AppContext';
 import { useParams } from 'react-router-dom';
+
 const Loader = () => {
     const {navigate}=useAppContext()
     const {nextUrl}=useParams()
@@ -12,10 +13,10 @@ const Loader = () => {
             }, 8000)
         }
     },[nextUrl])
+
   return (
     <div className='flex justify-center items-center h-screen' >
         <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'></div>
-      
     </div>
   )
 }
