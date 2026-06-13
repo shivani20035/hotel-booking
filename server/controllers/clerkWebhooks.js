457f3c1e83b4d05686fad539406b8ac1be79c2bc
Shivani Mishra <shivanimishra7860123@gmail.com>
2026-06-13 08:49:15 +0530

    all files verified

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -19,27 +19,19 @@ const clerkWebhooks = async(req,res)=>{
 
         //getting data from request body
         const {data,type}=req.body
-        
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses?.[0]?.email_address || "test@email.com",
+             username: (data.first_name || "Test") + " " + (data.last_name || "User"),
+            image: data.image_url || "https://placehold.co/150",
+}
         //Switch cases for diffrent event
         switch(type){
-
             case "user.created":{
-                const userData = {
-                    _id: data.id,
-                    email: data.email_addresses?.[0]?.email_address || "test@email.com",
-                    username: (data.first_name || "Test") + " " + (data.last_name || "User"),
-                    image: data.image_url || "https://placehold.co/150",
-                }
                 await User.create(userData);
                 break;
             }
             case "user.updated":{
-                 const userData = {
-                    _id: data.id,
-                    email: data.email_addresses?.[0]?.email_address || "test@email.com",
-                    username: (data.first_name || "Test") + " " + (data.last_name || "User"),
-                    image: data.image_url || "https://placehold.co/150",
-                }
                 await User.findByIdAndUpdate(data.id,userData);
                 break;
             }
